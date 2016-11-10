8d24ff3a207ad953013743d296181a60f5a25a57
MyNameIsntADAM <hazelton14113@yahoo.com>
2016-11-10 01:33:20 -0800

    -Fixed social links -Fixed pictures and titles -Added bees needs modals
    TODO:
    -Finish bees needs modals
    -Add shopping cart function
    -Add commercial
    -Add Twitter feed
    -Add VEI checkout

--- js/shop.js
+++ js/shop.js
@@ -6,14 +6,14 @@
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 100)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
+	$('body').scrollspy({
+        target: 'left-scroll',
         offset: 51
     });
 
